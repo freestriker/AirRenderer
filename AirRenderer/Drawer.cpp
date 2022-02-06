04f8b645e3dfb45ca148630b98a6c0537877b790
FREEstriker <2317416569@qq.com>
2022-02-07 00:43:18 +0800

    增加检测边缘绘制方法

--- AirRenderer/Drawer.cpp
+++ AirRenderer/Drawer.cpp
@@ -138,3 +138,96 @@ void Drawer::DrawTriangle(ivec2 positionA, ivec2 positionB, ivec2 positionC, Col
 		}
 	}
 }
+
+void Drawer::DrawTriangle_CheckBoundry(ivec2 positionA, ivec2 positionB, ivec2 positionC, Color color, Buffer<Color>& buffer, int width, int height)
+{
+	if (positionA.y > positionC.y)
+	{
+		ivec2 t = positionA;
+		positionA = positionC;
+		positionC = t;
+	}
+	if (positionB.y > positionC.y)
+	{
+		ivec2 t = positionB;
+		positionB = positionC;
+		positionC = t;
+	}
+	if (positionA.y > positionB.y)
+	{
+		ivec2 t = positionA;
+		positionA = positionB;
+		positionB = t;
+	}
+
+	int acHeight = positionC.y - positionA.y;
+	int abHeight = positionB.y - positionA.y;
+	int bcHeight = positionC.y - positionB.y;
+	ivec2 ac = positionC - positionA;
+	ivec2 ab = positionB - positionA;
+	ivec2 bc = positionC - positionB;
+	for (int y = positionA.y; y < positionB.y; y++)
+	{
+		int m = positionA.x + ac.x * (double(y - positionA.y) / double(acHeight));
+		int n = positionA.x + ab.x * (double(y - positionA.y) / double(abHeight));
+		int minX = std::min(m, n);
+		int maxX = std::max(m, n);
+		for (int x = minX; x <= maxX; x++)
+		{
+			if(CheckInBoundry(ivec2(x, y), width, height)) buffer.SetData(color, x, y);
+		}
+	}
+	if (abHeight == 0)
+	{
+		int minX = std::min(positionA.x, positionB.x);
+		int maxX = std::max(positionA.x, positionB.x);
+		for (int x = minX; x <= maxX; x++)
+		{
+			if (CheckInBoundry(ivec2(x, positionB.y), width, height)) buffer.SetData(color, x, positionB.y);
+		}
+	}
+	else if (bcHeight == 0)
+	{
+		int minX = std::min(positionC.x, positionB.x);
+		int maxX = std::max(positionC.x, positionB.x);
+		for (int x = minX; x <= maxX; x++)
+		{
+			if (CheckInBoundry(ivec2(x, positionB.y), width, height)) buffer.SetData(color, x, positionB.y);
+		}
+	}
+	else
+	{
+		int m = positionC.x + ac.x * (double(positionB.y - positionC.y) / double(acHeight));
+		int n = positionB.x;
+		int minX = std::min(m, n);
+		int maxX = std::max(m, n);
+		for (int x = minX; x <= maxX; x++)
+		{
+			if (CheckInBoundry(ivec2(x, positionB.y), width, height)) buffer.SetData(color, x, positionB.y);
+		}
+	}
+	for (int y = positionC.y; y > positionB.y; y--)
+	{
+		int m = positionC.x + ac.x * (double(y - positionC.y) / double(acHeight));
+		int n = positionC.x + bc.x * (double(y - positionC.y) / double(bcHeight));
+		int minX = std::min(m, n);
+		int maxX = std::max(m, n);
+		for (int x = minX; x <= maxX; x++)
+		{
+			if (CheckInBoundry(ivec2(x, y), width, height)) buffer.SetData(color, x, y);
+		}
+	}
+}
+
+bool Drawer::CheckInBoundry(ivec2 position, int width, int height)
+{
+	if (0 <= position.x && position.x < width
+		&& 0 <= position.y && position.y < height)
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
--- AirRenderer/Drawer.h
+++ AirRenderer/Drawer.h
@@ -2,7 +2,7 @@
 #include <glm/ext/vector_int2.hpp>
 #include "Color.h"
 #include "Buffer.h"
-
+#include "Configuration.h"
 using namespace glm;
 
 class Drawer
@@ -10,5 +10,8 @@ class Drawer
 public:
 	static void DrawLine(ivec2 startPosition, ivec2 endPosition, Color color, Buffer<Color>& buffer);
 	static void DrawTriangle(ivec2 positionA, ivec2 positionB, ivec2 positionC, Color color, Buffer<Color>& buffer);
+	static void DrawTriangle_CheckBoundry(ivec2 positionA, ivec2 positionB, ivec2 positionC, Color color, Buffer<Color>& buffer, int width, int height);
+private:
+	static bool CheckInBoundry(ivec2 position, int width, int height);
 };
 
