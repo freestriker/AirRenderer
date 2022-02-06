1ad781045d3b131bbda28b4a9ebb7e990cd7a822
FREEstriker <2317416569@qq.com>
2022-02-07 00:43:39 +0800

    增加渲染单个模型的方法

--- AirRenderer/MeshRenderer.cpp
+++ AirRenderer/MeshRenderer.cpp
@@ -2,13 +2,77 @@
 #include <QDebug>
 #include <qdir.h>
 #include <QCoreApplication>
+#include "Drawer.h"
 MeshRenderer::MeshRenderer()
 {
-	std::string s = (QCoreApplication::applicationDirPath() + '/' + "../../Model/Cube.obj").toStdString();
+	std::string s = (QCoreApplication::applicationDirPath() + '/' + "../../Model/Flat.obj").toStdString();
 	qDebug() << "Current working directory: " << QCoreApplication::applicationDirPath() << endl;
 	if (!OpenMesh::IO::read_mesh(mesh, s))
 	{
 		qDebug() << "Failed to load " << QString::fromStdString(s) << endl;
 		exit(1);
 	}
+}
+
+void MeshRenderer::Render(glm::mat4 mvpMatrix, glm::mat4 screenMatrix)
+{
+    for (Mesh::FaceIter f_it = mesh.faces_begin(); f_it != mesh.faces_end(); ++f_it)
+    {
+        std::vector<glm::vec4> vertexes = std::vector<glm::vec4>();
+        std::vector<float> ws = std::vector<float>();
+        int inCount = 0;
+        for (Mesh::FaceVertexIter fv_it = mesh.fv_iter(*f_it); fv_it.is_valid(); ++fv_it)
+        {
+            //±‰ªª
+            Mesh::Point p = mesh.point(*fv_it);
+            glm::vec4 pos = glm::vec4(p[0], p[1], p[2], 1);
+            pos = mvpMatrix * pos;
+
+            //≤‚ ‘Ãﬁ≥˝
+            if (-pos.w < pos.x && pos.x < pos.w
+                && -pos.w < pos.y && pos.y < pos.w
+                && 0 < pos.z && pos.z < pos.w)
+            {
+                inCount++;
+            }
+
+            //Õ∏ ”≥˝∑®
+            float w = pos.w;
+            ws.push_back(w);
+            pos = pos / w;
+
+            vertexes.push_back(pos);
+        }
+
+        //Ãﬁ≥˝
+        if (inCount == 0)
+        {
+
+        }
+        else
+        {
+            LogMatrix(screenMatrix);
+            std::vector<ivec2> pos = std::vector<ivec2>();
+            for (int i = 0; i < 3; i++)
+            {
+                vertexes[i] = screenMatrix * vertexes[i];
+                pos.push_back(ivec2(vertexes[i].x, vertexes[i].y));
+            }
+            Drawer::DrawTriangle_CheckBoundry(pos[0], pos[1], pos[2], Color::white, *configuration.colorBuffer, configuration.resolution.width, configuration.resolution.height);
+        }
+    }
+}
+void MeshRenderer::LogMatrix(glm::mat4 matrix)
+{
+    qDebug() << "Matrix :";
+    for (int i = 0; i < 4; i++)
+    {
+        QString s = "";
+        for (int j = 0; j < 4; j++)
+        {
+            s += QString::number(matrix[j][i], 'f', 6);
+            s += " ";
+        }
+        qDebug() << s;
+    }
 }
--- AirRenderer/MeshRenderer.h
+++ AirRenderer/MeshRenderer.h
@@ -2,6 +2,7 @@
 
 #include <OpenMesh/Core/IO/MeshIO.hh>
 #include <OpenMesh/Core/Mesh/TriMesh_ArrayKernelT.hh>
+#include <glm/mat4x4.hpp>
 typedef OpenMesh::TriMesh_ArrayKernelT<OpenMesh::DefaultTraits> Mesh;
 
 class MeshRenderer
@@ -9,5 +10,7 @@ class MeshRenderer
 public:
 	Mesh mesh;
 	MeshRenderer();
+	void Render(glm::mat4 mvpMatrix, glm::mat4 screenMatrix);
+	void LogMatrix(glm::mat4 matrix);
 };
 
