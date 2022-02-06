db2593f38bc03b111dce658f30923d5b0908a45c
FREEstriker <2317416569@qq.com>
2022-02-07 00:40:56 +0800

    增加显示窗口相关方法

--- AirRenderer/Configuration.cpp
+++ AirRenderer/Configuration.cpp
@@ -1,2 +1,31 @@
 #include "Configuration.h"
 Configuration configuration = Configuration();
+
+glm::mat4 Configuration::GetScreenMatrix()
+{
+    return glm::mat4(
+        float(resolution.width) / 2.0, 0, 0, 0,
+        0, float(-resolution.height) / 2.0, 0, 0,
+        0, 0, 1, 0,
+		float(resolution.width) / 2.0, float(resolution.height) / 2.0, 1, 1
+    );
+}
+void Configuration::Display()
+{
+	for (int x = 0; x < configuration.resolution.width; x++)
+	{
+		for (int y = 0; y < configuration.resolution.height; y++)
+		{
+			Color color = colorBuffer->GetData(x, y);
+			configuration.canvas.setPixelColor(x, y, QColor(color.r * color.a * 255, color.g * color.a * 255, color.b * color.a * 255));
+
+		}
+	}
+	configuration.label->setPixmap(QPixmap::fromImage(configuration.canvas));
+
+}
+
+Configuration::Configuration()
+{
+	this->colorBuffer = new Buffer<Color>(800, 450);
+}
--- AirRenderer/Configuration.h
+++ AirRenderer/Configuration.h
@@ -1,6 +1,10 @@
 #pragma once
 #include <QImage>
 #include <QLabel>
+#include "GameObject.h"
+#include "glm/mat4x4.hpp";
+#include "Drawer.h"
+#include "Color.h"
 
 typedef struct DisplayResolution
 {
@@ -15,6 +19,12 @@ public:
 	Resolution resolution;
 	QImage canvas;
 	QLabel* label;
+	Buffer<Color>* colorBuffer;
+
+	GameObject sceneObject;
+	glm::mat4 GetScreenMatrix();
+	void Display();
+	Configuration();
 };
 
 extern Configuration configuration;
