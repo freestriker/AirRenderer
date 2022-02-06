bab0d164f283d3f4262e4344720f60ba70c575d9
FREEstriker <2317416569@qq.com>
2022-02-07 00:44:26 +0800

    切换为渲染模型的输出

--- AirRenderer/RenderThread.cpp
+++ AirRenderer/RenderThread.cpp
@@ -1,22 +1,117 @@
 #include "RenderThread.h"
+#include <vector>;
+
+RenderThread::RenderThread(QObject* parent) :QThread(parent)
+{
+    timer = new QTimer(this);
+    renderer = new Renderer();
+    GameObject* camera = configuration.sceneObject.AddChild();
+    camera->camera = new Camera();
+    //camera->transform.translation.z += 5;
+
+    GameObject* go1 = configuration.sceneObject.AddChild();
+    go1->transform.translation.x += 5;
+
+    GameObject* go11 = go1->AddChild();
+    go11->transform.translation.y += 5;
+    go11->transform.translation.z -= 1;
+    go11->meshRenderer = new MeshRenderer();
+}
 
 void RenderThread::Run()
 {
     Render();
-    timer->setInterval(900);
-    connect(timer, SIGNAL(timeout()), this, SLOT(Render()));
-    timer->start();
-    this->exec();
+    //timer->setInterval(900);
+    //connect(timer, SIGNAL(timeout()), this, SLOT(Render()));
+    //timer->start();
+    //this->exec();
+}
+void RenderThread::GetCameras(std::vector<RenderItem<GameObject>>& vector)
+{
+    vector.clear();
+    for (GameObject::ChildIterator i = configuration.sceneObject.GetStartChildIterator(), end = configuration.sceneObject.GetEndChildIterator(); i != end; i++)
+    {
+        GetCamerasDFS(vector, *i, glm::mat4(1));
+    }
+
+}
+void RenderThread::GetCamerasDFS(std::vector<RenderItem<GameObject>>& vector, GameObject* gameObject, glm::mat4 parentMatrix)
+{
+    glm::mat4 matrix = parentMatrix * gameObject->transform.TranslationMatrix() * gameObject->transform.RotationMatrix();
+    LogMatrix(matrix);
+    for (GameObject::ChildIterator i = gameObject->GetStartChildIterator(), end = gameObject->GetEndChildIterator(); i != end; i++)
+    {
+        GetCamerasDFS(vector, *i, matrix);
+    }
+    if (gameObject->camera)
+    {
+        vector.push_back(RenderItem<GameObject>(gameObject, matrix));
+    }
+}
+
+void RenderThread::GetMeshRenderers(std::vector<RenderItem<GameObject>>& vector)
+{
+    vector.clear();
+    for (GameObject::ChildIterator i = configuration.sceneObject.GetStartChildIterator(), end = configuration.sceneObject.GetEndChildIterator(); i != end; i++)
+    {
+        GetMeshRenderersDFS(vector, *i, glm::mat4(1));
+    }
+
+}
+void RenderThread::GetMeshRenderersDFS(std::vector<RenderItem<GameObject>>& vector, GameObject* gameObject, glm::mat4 parentMatrix)
+{
+    glm::mat4 matrix = parentMatrix * gameObject->transform.TranslationMatrix() * gameObject->transform.RotationMatrix() * gameObject->transform.ScaleMatrix();
+    LogMatrix(matrix);
+    for (GameObject::ChildIterator i = gameObject->GetStartChildIterator(), end = gameObject->GetEndChildIterator(); i != end; i++)
+    {    
+        GetMeshRenderersDFS(vector, *i, matrix);
+    }
+    if (gameObject->meshRenderer)
+    {
+        vector.push_back(RenderItem<GameObject>(gameObject, matrix));
+    }
 }
 
 void RenderThread::Render()
 {
-    renderer->Render();
-    renderer->Display();
-}
+    //renderer->Render();
+    //renderer->Display();
 
-RenderThread::RenderThread(QObject* parent):QThread(parent)
+    std::vector<RenderItem<GameObject>> cameraItems = std::vector<RenderItem<GameObject>>();
+    GetCameras(cameraItems);
+    std::vector<RenderItem<GameObject>> meshRendererItems = std::vector<RenderItem<GameObject>>();
+    GetMeshRenderers(meshRendererItems);
+    for (RenderItem<GameObject> cameraItem : cameraItems)
+    {
+        
+        LogMatrix(cameraItem.transformationMatrix);
+        glm::mat4 observeMatrix = glm::inverse(cameraItem.transformationMatrix);
+        LogMatrix(observeMatrix);
+        glm::mat4 projectionMatrix = cameraItem.item->camera->ProjectionMatrix();
+        LogMatrix(projectionMatrix);
+        for (RenderItem<GameObject> meshRendererItem : meshRendererItems)
+        {
+            glm::mat4 mvpMatrix = projectionMatrix * observeMatrix * meshRendererItem.transformationMatrix;
+            LogMatrix(mvpMatrix);
+            glm::mat4 screenMatrix = configuration.GetScreenMatrix();
+            LogMatrix(screenMatrix);
+
+            meshRendererItem.item->meshRenderer->Render(mvpMatrix, screenMatrix);
+        }
+    }
+    configuration.Display();
+}
+void RenderThread::LogMatrix(glm::mat4 matrix)
 {
-    timer = new QTimer(this);
-    renderer = new Renderer();
+    qDebug() << "Matrix :";
+    for (int i = 0; i < 4; i++)
+    {
+        QString s = "";
+        for (int j = 0; j < 4; j++)
+        {
+            s += QString::number(matrix[j][i], 'f', 6);
+            s += " ";
+        }
+        qDebug() << s;
+    }
 }
--- AirRenderer/RenderThread.h
+++ AirRenderer/RenderThread.h
@@ -1,13 +1,22 @@
 #pragma once
 #include <QtWidgets>
 #include "Renderer.h"
+#include "Configuration.h"
+#include "glm/mat4x4.hpp"
+#include "RenderItem.h"
 
 class RenderThread : public QThread
 {
 	Q_OBJECT
 public:
+
 	QTimer* timer;
 	void Run();
+	void GetCameras(std::vector<RenderItem<GameObject>>& vector);
+	void GetCamerasDFS(std::vector<RenderItem<GameObject>>& vector, GameObject* gameObject, mat4 parentMatrix);
+	void GetMeshRenderers(std::vector<RenderItem<GameObject>>& vector);
+	void GetMeshRenderersDFS(std::vector<RenderItem<GameObject>>& vector, GameObject* gameObject, glm::mat4 parentMatrix);
+	void LogMatrix(glm::mat4 Matrix);
 	RenderThread(QObject* parent);
 	Renderer* renderer;
 private slots:void Render();
