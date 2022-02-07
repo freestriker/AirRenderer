1fa86e97168fe1f7b48554ecea187a024e5f4a66
FREEstriker <2317416569@qq.com>
2022-02-08 02:03:24 +0800

    修改面上下文

--- AirRenderer/FaceContext.cpp
+++ AirRenderer/FaceContext.cpp
@@ -1,1 +1,21 @@
 #include "FaceContext.h"
+#include "PixelIterator.h"
+
+glm::ivec4 FaceContext::GetFaceScreenSize()
+{
+    int minX = std::min(screenPosition[0].x, std::min(screenPosition[1].x, screenPosition[2].x));
+    int minY = std::min(screenPosition[0].y, std::min(screenPosition[1].y, screenPosition[2].y));
+    int maxX = std::max(screenPosition[0].x, std::max(screenPosition[1].x, screenPosition[2].x));
+    int maxY = std::max(screenPosition[0].y, std::max(screenPosition[1].y, screenPosition[2].y));
+    return glm::ivec4(minX, minY, maxX, maxY);
+}
+
+PixelIterator FaceContext::GetStartPixelIterator()
+{
+    return PixelIterator(this, true);
+}
+
+PixelIterator FaceContext::GetEndPixelIterator()
+{
+    return PixelIterator(this, false);
+}
--- AirRenderer/FaceContext.h
+++ AirRenderer/FaceContext.h
@@ -1,10 +1,16 @@
 #pragma once
-#include <glm/glm.hpp>
+#include <glm/ext/vector_int2.hpp>
+#include "VertexContext.h"
+class PixelIterator;
 class FaceContext
 {
 public:
-	glm::ivec2 position[3];
+	glm::ivec2 screenPosition[3];
 	float z[3];
 	float w[3];
+	VertexContext vertexContext[3];
+	glm::ivec4 GetFaceScreenSize();
+	PixelIterator GetStartPixelIterator();
+	PixelIterator GetEndPixelIterator();
 };
 
