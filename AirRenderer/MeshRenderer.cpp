34f2661edd9cbe22e83001d2ad64f1e5e382dd80
FREEstriker <2317416569@qq.com>
2022-02-09 21:14:01 +0800

    修改渲染流程

--- AirRenderer/MeshRenderer.cpp
+++ AirRenderer/MeshRenderer.cpp
@@ -1,39 +1,47 @@
 #include "MeshRenderer.h"
-#include <QDebug>
-#include <qdir.h>
-#include <QCoreApplication>
-#include "Drawer.h"
-#include "FaceContext.h"
-#include "PixelContext.h"
-#include "PixelIterator.h"
+#include "Configuration.h"
+
 MeshRenderer::MeshRenderer()
 {
 	std::string s = (QCoreApplication::applicationDirPath() + '/' + "../../Model/Triangle.obj").toStdString();
 	qDebug() << "Current working directory: " << QCoreApplication::applicationDirPath() << endl;
-	if (!OpenMesh::IO::read_mesh(mesh, s))
+    OpenMesh::IO::Options o = OpenMesh::IO::Options::ColorFloat + OpenMesh::IO::Options::VertexColor + OpenMesh::IO::Options::ColorAlpha;
+    mesh.request_vertex_colors();
+	if (!OpenMesh::IO::read_mesh(mesh, s, o, true))
 	{
 		qDebug() << "Failed to load " << QString::fromStdString(s) << endl;
 		exit(1);
 	}
+    shader = Shader();
 }
 
 void MeshRenderer::Render(glm::mat4 mvpMatrix, glm::mat4 screenMatrix)
 {
-    std::vector<VertexContext> vertexContext = std::vector<VertexContext>(mesh.vertices_end().handle().idx(), VertexContext());
+    std::vector<VertexOutContext> vertexOutContext = std::vector<VertexOutContext>(mesh.vertices_end().handle().idx(), VertexOutContext());
     for (Mesh::VertexIter v_it = mesh.vertices_sbegin(); v_it != mesh.vertices_end(); v_it++)
     {
-        //顶点着色器
+        int index = (*v_it).idx();
         Mesh::Point p = mesh.point(*v_it);
-        vertexContext[v_it.handle().idx()].position = mvpMatrix * glm::vec4(p[0], p[1], p[2], 1);
-        qDebug() << v_it.handle().idx() << endl;
+        //qDebug() << mesh.has_vertex_colors();
+        Mesh::Color c = mesh.vertex_colors()[index] / 255.0;
+        VertexInContext vertexInContext = VertexInContext();
+        vertexInContext.position = glm::vec4(p[0], p[1], p[2], 1);
+        //vertexInContext.color = Color::white;
+        vertexInContext.color = Color(c[0], c[1], c[2], 1);
+        vertexInContext.mvpMatrix = mvpMatrix;
+        vertexInContext.vertexIndex = index;
+
+        //顶点着色器
+        shader.VertexShading(vertexInContext, vertexOutContext[index]);
     }
     for (Mesh::FaceIter f_it = mesh.faces_begin(); f_it != mesh.faces_end(); ++f_it)
     {
         FaceContext faceContext = FaceContext();
-        int vertexIndex = 0, inBoundryCount = 0;
+        int index = 0, inBoundryCount = 0;
         for (Mesh::FaceVertexIter fv_it = mesh.fv_iter(*f_it); fv_it.is_valid(); ++fv_it)
         {
-            glm::vec4 pos = vertexContext[fv_it.handle().idx()].position;
+            int vertexIndex = (*fv_it).idx();
+            glm::vec4 pos = vertexOutContext[vertexIndex].position;
 
             //测试剔除
             if (-pos.w < pos.x && pos.x < pos.w
@@ -50,27 +58,36 @@ void MeshRenderer::Render(glm::mat4 mvpMatrix, glm::mat4 screenMatrix)
             //光栅化
             pos = screenMatrix * pos;
 
-            faceContext.screenPosition[vertexIndex] = ivec2(pos.x + 0.5, pos.y + 0.5);
-            faceContext.z[vertexIndex] = pos.z;
-            faceContext.w[vertexIndex] = pos.w;
-            faceContext.vertexContext[vertexIndex] = vertexContext[fv_it.handle().idx()];
-            vertexIndex++;
+            //图元装配
+            faceContext.screenPosition[index] = glm::ivec2(pos.x + 0.5, pos.y + 0.5);
+            faceContext.z[index] = pos.z;
+            faceContext.w[index] = pos.w;
+            faceContext.vertexIndex[index] = vertexIndex;
+            index++;
         }
         if (inBoundryCount > 0)
         {
-            for (PixelIterator pixelStartIterator = faceContext.GetStartPixelIterator(), pixelEndIterator = faceContext.GetEndPixelIterator()
-                ; pixelStartIterator != pixelEndIterator
-                ; pixelStartIterator++)
+            for (PixelIterator pixelIterator = PixelIterator(faceContext); pixelIterator.CheckValid(); pixelIterator++)
             {
-                if (pixelStartIterator.CheckInTriangle())
+                glm::ivec2 screenPosition = pixelIterator.GetScreenPosition();
+
+                if (0 <= screenPosition.x && screenPosition.x < configuration.resolution.width
+                    && 0 <= screenPosition.y && screenPosition.y < configuration.resolution.height)
                 {
+                    glm::dvec3 barycentricPosition = pixelIterator.GetBarycentricCoordinates();
+                    PixelInContext pixelInContext = PixelInContext();
+                    PixelOutContext pixelOutContext = PixelOutContext();
+                    pixelInContext.screenPosition = screenPosition;
+                    
+                    pixelInContext.position = vertexOutContext[faceContext.vertexIndex[0]].position * float(barycentricPosition.x) + vertexOutContext[faceContext.vertexIndex[1]].position * float(barycentricPosition.y) + vertexOutContext[faceContext.vertexIndex[2]].position * float(barycentricPosition.z);
+                    pixelInContext.color = vertexOutContext[faceContext.vertexIndex[0]].color * barycentricPosition.x + vertexOutContext[faceContext.vertexIndex[1]].color * barycentricPosition.y + vertexOutContext[faceContext.vertexIndex[2]].color * barycentricPosition.z;
+                    pixelInContext.w = faceContext.w[0] * barycentricPosition.x + faceContext.w[1] * barycentricPosition.y + faceContext.w[2] * barycentricPosition.z;
+                    pixelInContext.z = faceContext.z[0] * barycentricPosition.x + faceContext.z[1] * barycentricPosition.y + faceContext.z[2] * barycentricPosition.z;
+                    
                     //像素着色器
-                    PixelContext pixelContext = (*pixelStartIterator);
-                    if (0 <= pixelContext.screenPosition.x && pixelContext.screenPosition.x < configuration.resolution.width
-                        && 0 <= pixelContext.screenPosition.y && pixelContext.screenPosition.y < configuration.resolution.height)
-                    {
-                        configuration.colorBuffer->SetData(Color::white, pixelContext.screenPosition.x, pixelContext.screenPosition.y);
-                    }
+                    shader.PixelShading(pixelInContext, pixelOutContext);
+
+                    configuration.colorBuffer->SetData(pixelOutContext.color, screenPosition.x, screenPosition.y);
                 }
             }
         }
--- AirRenderer/MeshRenderer.h
+++ AirRenderer/MeshRenderer.h
@@ -1,15 +1,24 @@
 #pragma once
-
+#include <QDebug>
+#include <qdir.h>
+#include <QCoreApplication>
+#include "FaceContext.h"
+#include "PixelInContext.h"
+#include "PixelOutContext.h"
+#include "PixelIterator.h"
 #include <OpenMesh/Core/IO/MeshIO.hh>
 #include <OpenMesh/Core/Mesh/TriMesh_ArrayKernelT.hh>
 #include <glm/mat4x4.hpp>
-#include "VertexContext.h"
+#include "VertexInContext.h"
+#include "VertexOutContext.h"
 #include "FaceContext.h"
+#include "Shader.h"
 typedef OpenMesh::TriMesh_ArrayKernelT<OpenMesh::DefaultTraits> Mesh;
 
 class MeshRenderer
 {
 public:
+	Shader shader;
 	Mesh mesh;
 	MeshRenderer();
 	void Render(glm::mat4 mvpMatrix, glm::mat4 screenMatrix);
