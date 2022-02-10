9799446e4c1a9c9cd1a6c394cf0ce08aea4bbf09
FREEstriker <2317416569@qq.com>
2022-02-10 11:03:54 +0800

    添加FreeMesh库

--- AirRenderer/main.cpp
+++ AirRenderer/main.cpp
@@ -12,26 +12,6 @@ int main(int argc, char *argv[])
     AirRenderer w;
     w.show();
 
-    //GameObject go = GameObject();
-    //GameObject* c1 = go.AddChild();
-    //GameObject* c2 = go.AddChild();
-    //GameObject* c3 = go.AddChild();
-    //GameObject* c4 = go.AddChild();
-    //GameObject::ChildIterator si = go.GetStartChildIterator();
-    //GameObject::ChildIterator ei = go.GetEndChildIterator();
-    //int m = 0;
-    //for (GameObject::ChildIterator i = go.GetStartChildIterator(); i != ei; i++)
-    //{
-    //    m++;
-    //}
-    //GameObject::BrotherIterator bsi = c4->GetStartBrotherIterator();
-    //GameObject::BrotherIterator bei = c4->GetEndBrotherIterator();
-    //int n = 0;
-    //for (GameObject::BrotherIterator i = c4->GetStartBrotherIterator(); i != bei; i++)
-    //{
-    //    n++;
-    //}
-
     RenderThread* rt = new RenderThread(&a);
     rt->Run();
 
