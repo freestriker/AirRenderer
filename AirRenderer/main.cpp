ad0f6989d566770eb60394faf235be9b7fbe2462
FREEstriker <2317416569@qq.com>
2022-02-10 23:34:58 +0800

    精简包含目录

--- AirRenderer/main.cpp
+++ AirRenderer/main.cpp
@@ -1,17 +1,14 @@
 #include "AirRenderer.h"
 #include "stdafx.h"
 #include <QtWidgets/QApplication>
-#include "Renderer.h"
-#include "Configuration.h"
 #include "RenderThread.h"
 #include "GameObject.h"
 
-int main(int argc, char *argv[])
+int main(int argc, char* argv[])
 {
     QApplication a(argc, argv);
     AirRenderer w;
     w.show();
-
     RenderThread* rt = new RenderThread(&a);
     rt->Run();
 
