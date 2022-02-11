45e3caa8e329965a3d59f4941ba62f29f960d164
FREEstriker <2317416569@qq.com>
2022-02-12 03:19:43 +0800

    添加组件组

--- AirRenderer/GameObject.h
+++ AirRenderer/GameObject.h
@@ -3,12 +3,62 @@
 #include "Transform.h"
 #include "Camera.h"
 #include "MeshRenderer.h"
-class GameObject : public ChildBrotherTree<GameObject>
+#include "Object.h"
+#include "Component.h"
+class GameObject : public ChildBrotherTree<GameObject>, public Object
 {
 public:
+	std::string name;
 	Transform transform;
-	Camera* camera;
-	MeshRenderer* meshRenderer;
+	std::vector<Component*>* components;
 	GameObject();
+	GameObject(std::string name);
+	void UpdateSelf(void* data);
+	void UpdateSelfWithoutTransform(void* data);
+	void CascadeUpdate(void* data);
+
+	void AddComponent(Component* component);
+	template<typename T>
+	T* FindComponent(std::string typeName);
+	template<typename T>
+	T* RemoveComponent(std::string typeName);
+	void DestoryComponent(std::string typeName);
 };
 
+template<typename T>
+T* GameObject::FindComponent(std::string typeName)
+{
+	T* t = nullptr;
+	for each (Component * component in *components)
+	{
+		if (component->typeName == typeName)
+		{
+			t = static_cast<T*>(component);
+			break;
+		}
+	}
+	return t;
+}
+template<typename T>
+T* GameObject::RemoveComponent(std::string typeName)
+{
+	int index = -1;
+	for (int i = 0; i < components->size(); i++)
+	{
+		if (components->operator[](i)->typeName == typeName)
+		{
+			index = i;
+			break;
+		}
+	}
+	if (index >= 0)
+	{
+		T* t = static_cast<T*>(components->operator[](index));
+		components->erase(components->begin() + index);
+		return t;
+	}
+	else
+	{
+		return nullptr;
+	}
+}
